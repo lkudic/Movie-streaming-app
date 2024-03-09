f0a9532be95a72acf2c6bfe3f4937d5bb02f9de9
lkudic <78854649+lkudic@users.noreply.github.com>
2024-03-09 23:34:44 +0100

    Update Movies.js

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,5 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const titelUrl = "https://unogs-unogs-v1.p.rapidapi.com/search/titles";
+const [titles, setTitles] = useState([]);
+
+const fetchTitle = () => {
+  return axios.get(titleUrl).then((res) => setTitle(res.data));
+};
+
+useEffect(() => {
+  fetchTitle();
+}, []);
+
+
+
 const Movies = () => {
   return <h1>Movies</h1>;
 };
 
 export default Movies;
+
+
